1429df841b6bcabbac11a25de6ac5e0bd82fbb31
Marya Faivisovich <z89262554967@gmail.com>
2023-04-23 15:17:00 +0300

    почти кнопка

--- script/scripr.js
+++ script/scripr.js
@@ -1,3 +1,13 @@
-<script language="JavaScript">
-        alert ("Добро пожаловать на мою первую страницу!")
-</script>
+let name2 = "Добро пожаловать на мою первую страницу!";
+
+function hello(){
+        let result = "Привет! " + name2;
+        alert(result);
+}
+hello();
+
+if (confirm("Вам есть 18 лет?")){
+        alert("Добро пожаловать!");
+} else {
+        alert("Придется зайти немного попозже");
+}
