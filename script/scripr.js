ef4456eb94e1fa9f03ae217db871deb2e370e2bd
Marya Faivisovich <z89262554967@gmail.com>
2023-04-23 21:08:46 +0300

    готово

--- script/scripr.js
+++ script/scripr.js
@@ -20,10 +20,20 @@ function  getInput(){
                 alert(input);
         }
 }
+
 let page = document.querySelector(".page");
 let button1 = document.querySelector(".teme_button");
 button1.onclick = function(){
         page.classList.toggle("light_teme");
-        page.classList.toggle("dark_teme");
+       page.classList.toggle("dark_teme");
 
+}
+
+function getInput1(){
+        var input = document.getElementById("my_email").value;
+        if (document.getElementById("my_email").value === ""){
+                alert("Вы ввели не верный адрес");
+        }else{
+                alert("Ваше сообщение отправлено!");
+        }
 }
